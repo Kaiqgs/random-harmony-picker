3d3265de111fc7a13122a8408470ca79a05166ba
kaiqgs <kags_@live.com>
2024-09-27 12:14:48 -0300

    some

--- templates/main.js
+++ templates/main.js
@@ -5,32 +5,36 @@ function shuffleArray(array) {
     }
 }
 
-const options = [
+const optionsRight = [
     "<b>Bb</b>  <u>right</u> 🫱",
     "<b>C </b> <u>right</u> 🫱",
     "<b>A </b> <u>right</u> 🫱",
-    "<b>Bb</b>  <u>left</u> 🫲",
     "<b>F#</b>  <u>right</u> 🫱",
-    "<b>F </b> <u>left</u> 🫲",
     "<b>F </b> <u>right</u> 🫱",
     "<b>B </b> <u>right</u> 🫱",
     "<b>C#</b>  <u>right</u> 🫱",
     "<b>G </b> <u>right</u> 🫱",
+    "<b>E </b> <u>right</u> 🫱",
+    "<b>D </b> <u>right</u> 🫱",
+    "<b>Eb</b>  <u>right</u> 🫱",
+    "<b>Ab</b>  <u>right</u> 🫱",
+];
+const optionsLeft = [
+    "<b>Bb</b>  <u>left</u> 🫲",
+    "<b>F </b> <u>left</u> 🫲",
     "<b>A </b> <u>left</u> 🫲",
     "<b>C </b> <u>left</u> 🫲",
     "<b>F#</b>  <u>left</u> 🫲",
-    "<b>E </b> <u>right</u> 🫱",
     "<b>Ab</b>  <u>left</u> 🫲",
-    "<b>D </b> <u>right</u> 🫱",
-    "<b>Eb</b>  <u>right</u> 🫱",
     "<b>B </b> <u>left</u> 🫲",
     "<b>D </b> <u>left</u> 🫲",
-    "<b>Ab</b>  <u>right</u> 🫱",
     "<b>Eb</b>  <u>left</u> 🫲",
     "<b>C#</b>  <u>left</u> 🫲",
     "<b>G </b> <u>left</u> 🫲",
     "<b>E </b> <u>left</u> 🫲",
 ];
+
+const options = [...optionsRight, ...optionsLeft];
 
 const settings = {
     index: 0,
@@ -47,7 +51,16 @@ function display() {
 }
 function shuffle() {
     settings.index = 0;
-    shuffleArray(options);
+    shuffleArray(optionsRight);
+    shuffleArray(optionsLeft);
+    for (let i = 0; i < optionsLeft.length + optionsRight.length; i++) {
+        const index = Math.floor(i / 2);
+        if (i % 2 == 0) {
+            options[i] = optionsRight[index];
+            continue;
+        }
+        options[i] = optionsLeft[index];
+    }
     display();
 }
 
