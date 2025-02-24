0e5156b55e84713df99808af87f199ff02ace187
kaiqgs <kags_@live.com>
2025-02-24 17:08:01 -0300

    the more u learn

--- templates/main.js
+++ templates/main.js
@@ -1,3 +1,12 @@
+const MODIFIER_TAG = "{modifier}";
+const NAME_TAG = "{name}";
+const ICON_TAG = "{icon}";
+const MAJOR_PREFIX = "Maj ";
+const MINOR_PREFIX = "Min ";
+const RIGHT_NAME = "right";
+const LEFT_NAME = "left";
+const RIGHT_ICON = "🫱";
+const LEFT_ICON = "🫲";
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
@@ -5,36 +14,42 @@ function shuffleArray(array) {
     }
 }
 
-const optionsRight = [
-    "<b>Bb</b>  <u>right</u> 🫱",
-    "<b>C </b> <u>right</u> 🫱",
-    "<b>A </b> <u>right</u> 🫱",
-    "<b>F#</b>  <u>right</u> 🫱",
-    "<b>F </b> <u>right</u> 🫱",
-    "<b>B </b> <u>right</u> 🫱",
-    "<b>C#</b>  <u>right</u> 🫱",
-    "<b>G </b> <u>right</u> 🫱",
-    "<b>E </b> <u>right</u> 🫱",
-    "<b>D </b> <u>right</u> 🫱",
-    "<b>Eb</b>  <u>right</u> 🫱",
-    "<b>Ab</b>  <u>right</u> 🫱",
-];
-const optionsLeft = [
-    "<b>Bb</b>  <u>left</u> 🫲",
-    "<b>F </b> <u>left</u> 🫲",
-    "<b>A </b> <u>left</u> 🫲",
-    "<b>C </b> <u>left</u> 🫲",
-    "<b>F#</b>  <u>left</u> 🫲",
-    "<b>Ab</b>  <u>left</u> 🫲",
-    "<b>B </b> <u>left</u> 🫲",
-    "<b>D </b> <u>left</u> 🫲",
-    "<b>Eb</b>  <u>left</u> 🫲",
-    "<b>C#</b>  <u>left</u> 🫲",
-    "<b>G </b> <u>left</u> 🫲",
-    "<b>E </b> <u>left</u> 🫲",
-];
+function mapStrings(baseOptions, modifier, name, icon) {
+    const output = [];
+    for (let option of baseOptions) {
+        option = option.replace(MODIFIER_TAG, modifier);
+        option = option.replace(NAME_TAG, name);
+        option = option.replace(ICON_TAG, icon);
+        output.push(option);
+    }
+    return output;
+}
 
-const options = [...optionsRight, ...optionsLeft];
+const baseOptions = [
+    `${MODIFIER_TAG}<b>C </b> <u>${NAME_TAG}</u> <p class="display-6">${ICON_TAG}</p>`,
+    `${MODIFIER_TAG}<b>G </b> <u>${NAME_TAG}</u> <p class="display-6">${ICON_TAG}</p>`,
+    `${MODIFIER_TAG}<b>D </b> <u>${NAME_TAG}</u> <p class="display-6">${ICON_TAG}</p>`,
+    `${MODIFIER_TAG}<b>A </b> <u>${NAME_TAG}</u> <p class="display-6">${ICON_TAG}</p>`,
+    `${MODIFIER_TAG}<b>E </b> <u>${NAME_TAG}</u> <p class="display-6">${ICON_TAG}</p>`,
+    `${MODIFIER_TAG}<b>B </b> <u>${NAME_TAG}</u> <p class="display-6">${ICON_TAG}</p>`,
+    `${MODIFIER_TAG}<b>F </b> <u>${NAME_TAG}</u> <p class="display-6">${ICON_TAG}</p>`,
+    `${MODIFIER_TAG}<b>Bb</b>  <u>${NAME_TAG}</u> <p class="display-6">${ICON_TAG}</p>`,
+    `${MODIFIER_TAG}<b>Eb</b>  <u>${NAME_TAG}</u> <p class="display-6">${ICON_TAG}</p>`,
+    `${MODIFIER_TAG}<b>Ab</b>  <u>${NAME_TAG}</u> <p class="display-6">${ICON_TAG}</p>`,
+    `${MODIFIER_TAG}<b>F#</b>  <u>${NAME_TAG}</u> <p class="display-6">${ICON_TAG}</p>`,
+    `${MODIFIER_TAG}<b>C#</b>  <u>${NAME_TAG}</u> <p class="display-6">${ICON_TAG}</p>`,
+];
+const learnOptions = baseOptions.slice(0, 6);
+
+const optionsMajorRight = mapStrings(baseOptions, MAJOR_PREFIX, RIGHT_NAME, RIGHT_ICON);
+const optionsMajorLeft = mapStrings(baseOptions, MAJOR_PREFIX, LEFT_NAME, LEFT_ICON);
+const optionsMinorRight = mapStrings(learnOptions, MINOR_PREFIX, RIGHT_NAME, RIGHT_ICON);
+const optionsMinorLeft = mapStrings(learnOptions, MINOR_PREFIX, LEFT_NAME, LEFT_ICON);
+
+const optionsRight = [...optionsMajorRight, ...optionsMinorRight];
+const optionsLeft = [...optionsMajorLeft, ...optionsMinorLeft];
+
+const options = [...optionsRight, optionsLeft];
 
 const settings = {
     index: 0,
